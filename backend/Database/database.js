4a17e86e25a562a68d034331f0268d905f026199
Sachin Saxena <saxenasachin186@gmail.com>
2025-06-02 14:38:03 +0530

    commit databse

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-const mongoURI = process.env.MONGO_URI||'mongodb://localhost:27017/sessionbooking'
+const mongoURI ='mongodb+srv://sachinsaxenapec:<db_password>@sessionbooking.j37ge8m.mongodb.net/?retryWrites=true&w=majority&appName=sessionBooking'
 
 const connectTomongo = () => {
     mongoose.connect(mongoURI).then(() => console.log('MongoDB connected!'))
